34ecd70a0ec5a48999c5685c5465aba300d67ec8
Guillem Iscla Aragones <guillem@localhost.localdomain>
2025-08-24 12:26:36 +0200

    [Sprint 2][Task 3] SOLID design for UserStore

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -17,11 +17,11 @@ pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupReq
 
     let mut user_store = state.user_store.write().await;
 
-    if user_store.get_user(&user.email).is_ok() {
+    if user_store.get_user(&user.email).await.is_ok() {
         return Err(AuthAPIError::UserAlreadyExists);
     }
 
-    if user_store.add_user(user).is_err() {
+    if user_store.add_user(user).await.is_err() {
         return Err(AuthAPIError::UnexpectedError);
     }
 
--- auth-service/src/services/hashmap_user_store.rs
+++ auth-service/src/services/hashmap_user_store.rs
@@ -1,22 +1,15 @@
 use std::collections::HashMap;
 
-use crate::domain::User;
-
-#[derive(Debug, PartialEq)]
-pub enum UserStoreError {
-    UserAlreadyExists,
-    UserNotFound,
-    InvalidCredentials,
-    UnexpectedError,
-}
+use crate::domain::{data_stores::{UserStore, UserStoreError}, User};
 
 #[derive(Debug, Default)]
 pub struct HashmapUserStore {
     users: HashMap<String, User>,
 }
 
-impl HashmapUserStore {
-    pub fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
+#[async_trait::async_trait]
+impl UserStore for HashmapUserStore {
+    async fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
         if self.users.contains_key(&user.email) {
             Err(UserStoreError::UserAlreadyExists)
         } else {
@@ -25,15 +18,15 @@ impl HashmapUserStore {
         }
     }
 
-    pub fn get_user(&self, email: &str) -> Result<User, UserStoreError> {
+    async fn get_user(&self, email: &str) -> Result<User, UserStoreError> {
         match self.users.get(email) {
             Some(user) => Ok(user.clone()),
             None => Err(UserStoreError::UserNotFound),
         }
     }
 
-    pub fn validate_user(&self, email: &str, password: &str) -> Result<(), UserStoreError> {
-        match self.get_user(email) {
+    async fn validate_user(&self, email: &str, password: &str) -> Result<(), UserStoreError> {
+        match self.get_user(email).await {
             Ok(user) if user.password == password => Ok(()),
             Ok(_) => Err(UserStoreError::InvalidCredentials),
             Err(error) => Err(error), //we return the same UserNotFound error required
@@ -41,7 +34,7 @@ impl HashmapUserStore {
     }
 }
 
-// TODO: Add unit tests for your `HashmapUserStore` implementation
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,9 +48,9 @@ mod tests {
             false,
         );
         let same_user = user.clone();
-        assert!(hashmap_user_store.add_user(user).is_ok());
+        assert!(hashmap_user_store.add_user(user).await.is_ok());
         assert_eq!(
-            hashmap_user_store.add_user(same_user),
+            hashmap_user_store.add_user(same_user).await,
             Err(UserStoreError::UserAlreadyExists)
         );
     }
@@ -69,10 +62,10 @@ mod tests {
         let other_email = "other_person@letsgetrusty.com".to_string();
         let user = User::new(email.clone(), "RustIsSecure123".to_string(), false);
 
-        let _ = hashmap_user_store.add_user(user);
-        assert!(hashmap_user_store.get_user(&email).is_ok());
+        let _ = hashmap_user_store.add_user(user).await;
+        assert!(hashmap_user_store.get_user(&email).await.is_ok());
         assert_eq!(
-            hashmap_user_store.get_user(&other_email),
+            hashmap_user_store.get_user(&other_email).await,
             Err(UserStoreError::UserNotFound)
         );
     }
@@ -86,14 +79,14 @@ mod tests {
         let other_password = "OtherPassword456".to_string();
         let user = User::new(email.clone(), password.clone(), false);
 
-        let _ = hashmap_user_store.add_user(user);
-        assert!(hashmap_user_store.validate_user(&email, &password).is_ok());
+        let _ = hashmap_user_store.add_user(user).await;
+        assert!(hashmap_user_store.validate_user(&email, &password).await.is_ok());
         assert_eq!(
-            hashmap_user_store.validate_user(&email, &other_password),
+            hashmap_user_store.validate_user(&email, &other_password).await,
             Err(UserStoreError::InvalidCredentials)
         );
         assert_eq!(
-            hashmap_user_store.validate_user(&other_email, &other_password),
+            hashmap_user_store.validate_user(&other_email, &other_password).await,
             Err(UserStoreError::UserNotFound)
         );
     }
