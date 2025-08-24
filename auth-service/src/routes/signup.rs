5eac04e7159adb17066cefc55fc0fd68c9c5f6f2
Guillem Iscla Aragones <guillem@localhost.localdomain>
2025-08-24 12:04:43 +0200

    [Sprint 2][Task 2] sign-up error cases

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,20 +1,35 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
-use crate::{app_state::AppState, domain::User};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User},
+};
 
-pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupRequest>) -> impl IntoResponse  {
-    // Create a new `User` instance using data in the `request`
-    let user = User::new(request.email, request.password, request.requires_2fa);
+pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupRequest>) -> Result<impl IntoResponse, AuthAPIError>  {
+    let email = request.email;
+    let password = request.password;
+
+    if email.is_empty() || !email.contains('@') || password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials)
+    }
+
+    let user = User::new(email, password, request.requires_2fa);
 
     let mut user_store = state.user_store.write().await;
 
-    user_store.add_user(user).unwrap();
+    if user_store.get_user(&user.email).is_ok() {
+        return Err(AuthAPIError::UserAlreadyExists);
+    }
+
+    if user_store.add_user(user).is_err() {
+        return Err(AuthAPIError::UnexpectedError);
+    }
 
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
 
 #[derive(Deserialize)]
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,4 @@
-use auth_service::routes::SignupResponse;
+use auth_service::{routes::SignupResponse, ErrorResponse};
 
 use crate::helpers::{get_random_email, TestApp};
 
@@ -69,3 +69,89 @@ async fn should_return_201_if_valid_input() {
         expected_response
     );
 }
+
+//...
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let bad_email_1 = "";
+    let bad_email_2 = "user_name_a_domain";
+    let short_password = "short!";
+    let random_email = get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "email": bad_email_1,
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": bad_email_2,
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": short_password,
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            test_case
+        );
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    // Call the signup route twice. The second request should fail with a 409 HTTP status code    
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let sign_up_request = serde_json::json!({
+            "email": random_email,
+            "password": "Password123",
+            "requires2FA": true
+        });
+
+    //Login once
+    let _ = app.post_signup(&sign_up_request).await;
+
+    //Login twice
+    let response = app.post_signup(&sign_up_request).await;
+
+    assert_eq!(
+        response.status().as_u16(),
+        409,
+        "Failed for input: {:?}",
+        sign_up_request
+    );
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned()
+    );
+}
+
