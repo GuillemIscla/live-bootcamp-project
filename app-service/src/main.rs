fc4d927fecb602b0f839637c0f760ab3e2d1be0f
Guillem Iscla Aragones <guillem@localhost.localdomain>
2025-09-06 09:17:50 +0200

    [Sprint 3][Side Quest 3] Rervert back to old url until I have a domain

--- app-service/src/main.rs
+++ app-service/src/main.rs
@@ -86,7 +86,7 @@ async fn protected(jar: CookieJar) -> impl IntoResponse {
             StatusCode::UNAUTHORIZED.into_response()
         }
         reqwest::StatusCode::OK => Json(ProtectedRouteResponse {
-            img_url: "https://d3d1grgs1oo189.cloudfront.net/Light-Live-Bootcamp-Certificate.png".to_owned(),
+            img_url: "https://i.ibb.co/YP90j68/Light-Live-Bootcamp-Certificate.png".to_owned(),
         })
         .into_response(),
         _ => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
