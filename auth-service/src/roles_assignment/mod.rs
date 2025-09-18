09a396d4723fda315e0a78fdc444aeea0fc1ea35
Guillem Iscla Aragones <guillem@localhost.localdomain>
2025-09-18 17:43:15 +0200

    [Sprint 6][Cleanup] Deactivate code under development

--- auth-service/src/roles_assignment/mod.rs
+++ auth-service/src/roles_assignment/mod.rs
@@ -1,6 +1,6 @@
-pub mod roles;
-pub use roles::*;
-pub mod roles_middleware;
-pub use roles_middleware::*;
-pub mod roles_extractors;
-pub use roles_extractors::*;
+// pub mod roles;
+// pub use roles::*;
+// pub mod roles_middleware;
+// pub use roles_middleware::*;
+// pub mod roles_extractors;
+// pub use roles_extractors::*;
